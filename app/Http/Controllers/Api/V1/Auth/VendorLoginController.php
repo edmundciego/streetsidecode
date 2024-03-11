eb0a28cea56cb2fc1b46716a59f8971992871332
Edmund Ciego <edmundciego@users.noreply.github.com>
2024-03-10 19:29:38 -0600

    Your Message

---
+++ app/Http/Controllers/Api/V1/Auth/VendorLoginController.php
@@ -0,0 +1,231 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use App\CentralLogics\Helpers;
+use App\Models\BusinessSetting;
+use App\Http\Controllers\Controller;
+use App\Models\Vendor;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use App\Models\Zone;
+use App\Models\Store;
+use App\CentralLogics\StoreLogic;
+use App\Models\Admin;
+use App\Models\Translation;
+use App\Models\VendorEmployee;
+use MatanYadaev\EloquentSpatial\Objects\Point;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Validation\Rules\Password;
+
+class VendorLoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required|min:6'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => Helpers::error_processor($validator)], 403);
+        }
+        $vendor_type= $request->vendor_type;
+
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if($vendor_type == 'owner'){
+            if (auth('vendor')->attempt($data)) {
+                $token = $this->genarate_token($request['email']);
+                $vendor = Vendor::where(['email' => $request['email']])->first();
+                if($vendor->stores[0]->status == 0 && $vendor->status == 0)
+                {
+                    return response()->json([
+                        'errors' => [
+                            ['code' => 'auth-002', 'message' => translate('messages.Your_registration_is_not_approved_yet._You_can_login_once_admin_approved_the_request')]
+                        ]
+                    ], 403);
+                } elseif($vendor->stores[0]->status == 0 && $vendor->status == 1){
+                    return response()->json([
+                        'errors' => [
+                            ['code' => 'auth-002', 'message' => translate('messages.Your_account_is_suspended')]
+                        ]
+                    ], 403);
+                }
+
+                $vendor->auth_token = $token;
+                $vendor->save();
+                return response()->json(['token' => $token, 'zone_wise_topic'=> $vendor->stores[0]->zone->store_wise_topic], 200);
+            }  else {
+                $errors = [];
+                array_push($errors, ['code' => 'auth-001', 'message' => translate('Credential_do_not_match,_please_try_again')]);
+                return response()->json([
+                    'errors' => $errors
+                ], 401);
+            }
+        }elseif($vendor_type == 'employee'){
+
+            if (auth('vendor_employee')->attempt($data)) {
+                $token = $this->genarate_token($request['email']);
+                $vendor = VendorEmployee::where(['email' => $request['email']])->first();
+                if($vendor->stores[0]->status == 0 && $vendor->status == 0)
+                {
+                    return response()->json([
+                        'errors' => [
+                            ['code' => 'auth-002', 'message' => translate('messages.Your_registration_is_not_approved_yet._You_can_login_once_admin_approved_the_request')]
+                        ]
+                    ], 403);
+                }
+                elseif($vendor->stores[0]->status == 0 && $vendor->status == 1){
+                    return response()->json([
+                        'errors' => [
+                            ['code' => 'auth-002', 'message' => translate('messages.Your_account_is_suspended')]
+                        ]
+                    ], 403);
+                }
+                $vendor->auth_token = $token;
+                $vendor->save();
+                $role = $vendor->role ? json_decode($vendor->role->modules):[];
+                return response()->json(['token' => $token, 'zone_wise_topic'=> $vendor->store->zone->store_wise_topic, 'role'=>$role], 200);
+            } else {
+                $errors = [];
+                array_push($errors, ['code' => 'auth-001', 'message' => translate('Credential_do_not_match,_please_try_again')]);
+                return response()->json([
+                    'errors' => $errors
+                ], 401);
+            }
+        } else {
+            $errors = [];
+            array_push($errors, ['code' => 'auth-001', 'message' => translate('Credential_do_not_match,_please_try_again')]);
+            return response()->json([
+                'errors' => $errors
+            ], 401);
+        }
+
+    }
+
+    private function genarate_token($email)
+    {
+        $token = Str::random(120);
+        $is_available = Vendor::where('auth_token', $token)->where('email', '!=', $email)->count();
+        if($is_available)
+        {
+            $this->genarate_token($email);
+        }
+        return $token;
+    }
+
+    public function register(Request $request)
+    {
+        $status = BusinessSetting::where('key', 'toggle_store_registration')->first();
+        if(!isset($status) || $status->value == '0')
+        {
+            return response()->json(['errors' => Helpers::error_processor('self-registration', translate('messages.store_self_registration_disabled'))]);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'f_name' => 'required|max:100',
+            'l_name' => 'nullable|max:100',
+            // 'name' => 'required|max:191',
+            // 'address' => 'required|max:1000',
+            'latitude' => 'required',
+            'longitude' => 'required',
+            'email' => 'required|unique:vendors',
+            'phone' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:10|max:20|unique:vendors',
+            'minimum_delivery_time' => 'required',
+            'maximum_delivery_time' => 'required',
+            'delivery_time_type'=>'required',
+            'password' => ['required', Password::min(8)->mixedCase()->letters()->numbers()->symbols()->uncompromised()],
+            'zone_id' => 'required',
+            'module_id' => 'required',
+            'logo' => 'required',
+            'tax' => 'required'
+        ],[
+            'password.required' => translate('The password is required'),
+            'password.min_length' => translate('The password must be at least :min characters long'),
+            'password.mixed' => translate('The password must contain both uppercase and lowercase letters'),
+            'password.letters' => translate('The password must contain letters'),
+            'password.numbers' => translate('The password must contain numbers'),
+            'password.symbols' => translate('The password must contain symbols'),
+            'password.uncompromised' => translate('The password is compromised. Please choose a different one'),
+        ]);
+
+        if($request->zone_id)
+        {
+            $zone = Zone::query()
+            ->whereContains('coordinates', new Point($request->latitude, $request->longitude, POINT_SRID))
+            ->where('id',$request->zone_id)
+            ->first();
+            if(!$zone){
+                $validator->getMessageBag()->add('latitude', translate('messages.coordinates_out_of_zone'));
+                return response()->json(['errors' => Helpers::error_processor($validator)], 403);
+            }
+        }
+
+        $data = json_decode($request->translations, true);
+
+        if (count($data) < 1) {
+            $validator->getMessageBag()->add('translations', translate('messages.Name and description in english is required'));
+        }
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => Helpers::error_processor($validator)], 403);
+        }
+        $vendor = new Vendor();
+        $vendor->f_name = $request->f_name;
+        $vendor->l_name = $request->l_name;
+        $vendor->email = $request->email;
+        $vendor->phone = $request->phone;
+        $vendor->password = bcrypt($request->password);
+        $vendor->status = null;
+        $vendor->save();
+
+        $store = new Store;
+        $store->name = $data[0]['value'];
+        $store->phone = $request->phone;
+        $store->email = $request->email;
+        $store->logo = Helpers::upload('store/', 'png', $request->file('logo'));
+        $store->cover_photo = Helpers::upload('store/cover/', 'png', $request->file('cover_photo'));
+        $store->address = $data[1]['value'];
+        $store->latitude = $request->latitude;
+        $store->longitude = $request->longitude;
+        $store->vendor_id = $vendor->id;
+        $store->zone_id = $request->zone_id;
+        $store->tax = $request->tax;
+        $store->delivery_time = $request->minimum_delivery_time .'-'. $request->maximum_delivery_time.' '.$request->delivery_time_type;
+        $store->module_id = $request->module_id;
+        $store->status = 0;
+        $store->save();
+        $store->module->increment('stores_count');
+        if(config('module.'.$store->module->module_type)['always_open'])
+        {
+            StoreLogic::insert_schedule($store->id);
+        }
+
+        foreach ($data as $key=>$i) {
+            $data[$key]['translationable_type'] = 'App\Models\Store';
+            $data[$key]['translationable_id'] = $store->id;
+        }
+        Translation::insert($data);
+
+        try{
+            $admin= Admin::where('role_id', 1)->first();
+            $mail_status = Helpers::get_mail_status('registration_mail_status_store');
+            if(config('mail.status') && $mail_status == '1'){
+                Mail::to($request['email'])->send(new \App\Mail\VendorSelfRegistration('pending', $vendor->f_name.' '.$vendor->l_name));
+            }
+            $mail_status = Helpers::get_mail_status('store_registration_mail_status_admin');
+            if(config('mail.status') && $mail_status == '1'){
+                Mail::to($admin['email'])->send(new \App\Mail\StoreRegistration('pending', $vendor->f_name.' '.$vendor->l_name));
+            }
+        }catch(\Exception $ex){
+            info($ex->getMessage());
+        }
+
+        return response()->json(['message'=>translate('messages.application_placed_successfully')],200);
+    }
+}
